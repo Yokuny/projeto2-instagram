4b6f852c55fecffcbad14bc56c22d49972a12117
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2022-12-19 21:43:12 -0300

    s

---
+++ stories.js
@@ -0,0 +1,37 @@
+const storiesField = document.getElementById("storiesSize");
+const instagramStoryCircle = document.createElement("img");
+instagramStoryCircle.src = "./src/stories_background.jpg";
+const storieAvatarPicture = document.createElement("img");
+storieAvatarPicture.src = "./src/tab-icon.png";
+const avatarName = document.createElement("p");
+avatarName.textContent = "nome";
+const aDiv = document.createElement("div");
+
+aDiv.appendChild(instagramStoryCircle);
+aDiv.appendChild(storieAvatarPicture);
+aDiv.appendChild(avatarName);
+storiesField.appendChild(aDiv);
+storiesField.appendChild(aDiv);
+storiesField.appendChild(aDiv);
+
+/*
+<div>
+    <img src="./src/stories_background.jpg" alt="">
+    <img src="./src/tab-icon.png" alt="">
+    <p>pessoa</p>
+</div>
+<div>
+    <div>
+        <img src="./src/stories_background.jpg" alt="">
+        <img src="./src/tab-icon.png" alt="">
+    </div>
+    <p>pessoa</p>
+</div>
+<div>
+    <div>
+        <img src="./src/stories_background.jpg" alt="">
+        <img src="./src/tab-icon.png" alt="">
+    </div>
+    <p>pessoa</p>
+</div>
+ */
