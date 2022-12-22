63426a066a2e3ad7eafcaf54d5c98f57dfee85cc
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2022-12-22 17:27:54 -0300

    Js refatored

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
-function coracao(id){
-    const coracaoVazio = document.getElementById(id);
-    coracaoVazio.innerHTML = ""
-    const coracaoCheio = document.createElement("img");
-    coracaoCheio.src = "./src/heart-svgrepo-com.svg";
-    coracaoVazio.append(coracaoCheio);
+function coracao(id) {
+  const coracaoCheio = document.createElement("img");
+  coracaoCheio.src = "./src/heart-svgrepo-com.svg";
+  const coracaoVazio = document.getElementById(id);
+  coracaoVazio.innerHTML = "";
+  coracaoVazio.append(coracaoCheio);
 }
-function salvar(id){
-    const bookmarkVazio = document.getElementById(id);
-    bookmarkVazio.innerHTML = ""
-    const bookmarkFeito = document.createElement("img");
-    bookmarkFeito.src = "./src/bookmark-svgrepo-com.svg";
-    bookmarkVazio.append(bookmarkFeito);
+function salvar(id) {
+  const bookmarkFeito = document.createElement("img");
+  bookmarkFeito.src = "./src/bookmark-svgrepo-com.svg";
+  const bookmarkVazio = document.getElementById(id);
+  bookmarkVazio.innerHTML = "";
+  bookmarkVazio.append(bookmarkFeito);
 }
-function comentar(id){
-    const divThatHoldEverything = document.createElement("div");
-    divThatHoldEverything.classList.add("newComment");
-    const divForTheNewElements = document.createElement("div");
-    divForTheNewElements.classList.add("theComment");
-    const justDiv = document.createElement("div");
-    const userNameH4 = document.createElement("h4");
-    userNameH4.textContent = "iso.profile";
-    const userComentP = document.createElement("p");
-    userComentP.textContent = document.getElementById(id).value
-    document.getElementById(id).value = '';
-    const heartIcon = document.createElement("div");
-    heartIcon.innerHTML = '<ion-icon name="heart-outline"></ion-icon>';
-    justDiv.appendChild(userNameH4);
-    justDiv.appendChild(userComentP);
-    divForTheNewElements.appendChild(justDiv);
-    divForTheNewElements.appendChild(heartIcon);
-    divThatHoldEverything.appendChild(divForTheNewElements);
-    document.getElementById(`comentField-id-${id}`).appendChild(divThatHoldEverything);
-/*
+function comentar(id) {
+  const userNameH4 = document.createElement("h4");
+  userNameH4.textContent = "iso.profile";
+
+  const userComentP = document.createElement("p");
+  userComentP.textContent = document.getElementById(id).value;
+  document.getElementById(id).value = "";
+
+  const heartIcon = document.createElement("div");
+  heartIcon.innerHTML = '<ion-icon name="heart-outline"></ion-icon>';
+
+  const justDiv = document.createElement("div");
+  justDiv.appendChild(userNameH4);
+  justDiv.appendChild(userComentP);
+
+  const divForTheNewElements = document.createElement("div");
+  divForTheNewElements.classList.add("theComment");
+  divForTheNewElements.appendChild(justDiv);
+  divForTheNewElements.appendChild(heartIcon);
+
+  const divThatHoldEverything = document.createElement("div");
+  divThatHoldEverything.classList.add("newComment");
+  divThatHoldEverything.appendChild(divForTheNewElements);
+  document.getElementById(`comentField-id-${id}`).appendChild(divThatHoldEverything);
+  /*
     <div id="comentField-id-rrbbh">
 
         <div id="comentField-id-rrbbh">
