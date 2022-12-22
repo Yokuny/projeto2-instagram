2b0ad8bbd938fb41059bec902352101725f2d8dd
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2022-12-22 12:29:50 -0300

    the function to add comments is made
    and all is working fine. the project is done

--- script.js
+++ script.js
@@ -13,5 +13,37 @@ function salvar(id){
     bookmarkVazio.append(bookmarkFeito);
 }
 function comentar(id){
-    
+    const divThatHoldEverything = document.createElement("div");
+    divThatHoldEverything.classList.add("newComment");
+    const divForTheNewElements = document.createElement("div");
+    divForTheNewElements.classList.add("theComment");
+    const justDiv = document.createElement("div");
+    const userNameH4 = document.createElement("h4");
+    userNameH4.textContent = "iso.profile";
+    const userComentP = document.createElement("p");
+    userComentP.textContent = document.getElementById(id).value
+    document.getElementById(id).value = '';
+    const heartIcon = document.createElement("div");
+    heartIcon.innerHTML = '<ion-icon name="heart-outline"></ion-icon>';
+    justDiv.appendChild(userNameH4);
+    justDiv.appendChild(userComentP);
+    divForTheNewElements.appendChild(justDiv);
+    divForTheNewElements.appendChild(heartIcon);
+    divThatHoldEverything.appendChild(divForTheNewElements);
+    document.getElementById(`comentField-id-${id}`).appendChild(divThatHoldEverything);
+/*
+    <div id="comentField-id-rrbbh">
+
+        <div id="comentField-id-rrbbh">
+
+            <div class="newComment">
+                <div class="theComment">
+                    <div>
+                        <h4>yokuny</h4><p>What's your method for sharing the project with the client?</p>
+                    </div>
+                    <ion-icon name="heart-outline"></ion-icon>
+                </div>
+            </div>
+*/
+
 }
